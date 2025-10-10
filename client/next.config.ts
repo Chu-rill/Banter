011de8f045e76561f45ca6a746dfb5b9519e8f9f
Chu-rill <churchillrimamskep@gmail.com>
2025-10-10 08:50:28 -0400

    refactor:next config

--- client/next.config.ts
+++ client/next.config.ts
@@ -1,7 +1,8 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  // remove output: "standalone"
+  pageExtensions: ["js", "jsx", "ts", "tsx"],
 };
 
 export default nextConfig;
